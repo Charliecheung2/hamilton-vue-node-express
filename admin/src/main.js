e77f54486d8077617825f96cb78807822c1601eb
Charliecheung2 <84767967@qq.com>
2019-09-02 23:19:13 +0800

    完成了管理页面的制作，学会了如何连接数据库，如何通过model和路由来访问数据，学了通用CRUD的写法

---
+++ admin/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './plugins/element.js'
+import router from './router.js'
+import http from './http.js'
+
+
+Vue.config.productionTip = false
+Vue.prototype.$http = http
+
+
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app')
